99f197b88730162daf7f1247cd376d9ba55b202e
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-07-23 13:05:25 +0200

    Add block settings

--- src/main/kotlin/org/wdfeer/skulk_burner/block/SkulkBurnerBlock.kt
+++ src/main/kotlin/org/wdfeer/skulk_burner/block/SkulkBurnerBlock.kt
@@ -2,7 +2,12 @@ package org.wdfeer.skulk_burner.block
 
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.minecraft.block.Block
+import net.minecraft.block.Blocks
+import net.minecraft.block.MapColor
 
-class SkulkBurnerBlock : Block(FabricBlockSettings.create()) {
+class SkulkBurnerBlock : Block(
+    FabricBlockSettings.create().mapColor(MapColor.PALE_PURPLE)
+        .strength(3.5f)
+        .luminance(Blocks.createLightLevelFromLitBlockState(10))) {
 
 }
