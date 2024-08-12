b063c90d08e916df945f6e2a03be1b4d7df63c62
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-08-12 08:56:08 +0200

    Fix flower being opaque

--- src/client/kotlin/org/wdfeer/fuel_to_xp/FuelToXpClient.kt
+++ src/client/kotlin/org/wdfeer/fuel_to_xp/FuelToXpClient.kt
@@ -1,9 +1,11 @@
 package org.wdfeer.fuel_to_xp
 
 import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap
+import net.minecraft.client.render.RenderLayer
 
 object FuelToXpClient : ClientModInitializer {
 	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		BlockRenderLayerMap.INSTANCE.putBlock(FuelToXp.block, RenderLayer.getCutout());
 	}
 }
